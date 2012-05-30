41cc7a0c602997582424905b89185ef708cf173c
Markus Paeschke <Fraggy_Boy@web.de>
2012-05-30 14:27:20 +0300

    Cached $(this). Changed the name of the css_class.

--- a_better_placeholder_fix.js
+++ a_better_placeholder_fix.js
@@ -1,44 +1,53 @@
+/*
+* jQuery flip addon
+* By: Markus Paeschke
+* Version 0.1
+* Last Modified: 30.05.2012
+*/
+
 (function($){
 	$.fn.placeholder = function(options){
 		var defaults = {
-			css_class:'placeholder'
+			css_class : 'jPlaceholder'
 		};
 		var options = $.extend(defaults,options);
 
 		this.each(function(){
-			if($(this).attr('placeholder') !== undefined){
-				var phvalue = $(this).attr('placeholder');
-				var currvalue = $(this).attr('value');
+			var $this = $(this);
+			if($this.attr('placeholder') !== undefined){
+				var phvalue   = $this.attr('placeholder');
+				var currvalue = $this.attr('value');
 
 				if(phvalue == currvalue){
-					$(this).addClass(options.css_class);
+					$this.addClass(options.css_class);
 				}
 
 				if(currvalue == ''){
-					$(this).addClass(options.css_class);
-					$(this).val(phvalue);
+					$this
+						.addClass(options.css_class)
+						.val(phvalue);
 				}
 
-				$(this).focusin(function(){
-					var ph = $(this).attr('placeholder');
+				$this.focusin(function(){
+					var ph = $this.attr('placeholder');
 
-					if(ph == $(this).val()){
-						$(this).val('').removeClass(options.css_class);
+					if(ph == $this.val()){
+						$this.val('').removeClass(options.css_class);
 					}
 				});
 
-				$(this).focusout(function(){
-					var ph = $(this).attr('placeholder');
+				$this.focusout(function(){
+					var ph = $this.attr('placeholder');
 
-					if($(this).val() == ''){
-						$(this).val(ph).addClass(options.css_class);
+					if($this.val() == ''){
+						$this.val(ph).addClass(options.css_class);
 					}
 				});
 
 				$('form').submit(function(){
-					$(this).find('input, textarea').each(function(){
-						if($(this).val() == $(this).attr('placeholder')){
-							$(this).val('');
+					$this.find('input, textarea').each(function(){
+						if($this.val() == $this.attr('placeholder')){
+							$this.val('');
 						}
 					});
 				});
@@ -47,10 +56,3 @@
 		return this;
 	}
 })(jQuery);
-
-$(document).ready(function(){
-	if($.browser.msie || $.browser.opera || $.browser.mozilla){
-		//($.browser.mozilla && $.browser.version <= '1.9.2.12')
-		$('input, textarea').placeholder();
-	}
-});
